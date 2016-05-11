55b05278bd9cb911483a4dad8b7ce4089acaa8c9
Mykola Gurov <ngurov@gmail.com>
2016-05-11 22:06:57 +0200

    testy test

--- progress_test.go
+++ progress_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gosuri/uiprogress"
 	"time"
 	"os"
+	"testing"
 )
 
 func ExampleStoppingPrintout() {
@@ -19,4 +20,32 @@ func ExampleStoppingPrintout() {
 	time.Sleep(1 * time.Second)
 	// Output: [====================================================================]
 
+}
+
+func TestStoppingPrintout(t *testing.T) {
+
+	counter := countingWriter{}
+
+	progress := uiprogress.New()
+	progress.RefreshInterval = time.Millisecond * 10
+	progress.Out = &counter
+	bar := progress.AddBar(1)
+	progress.Start()
+	bar.Incr()
+	time.Sleep(time.Millisecond * 15)
+	progress.Stop()
+	time.Sleep(1 * time.Second)
+
+	if 1 != counter.Count {
+		t.Errorf("Expected count 1, actual: %d", counter.Count)
+	}
+}
+
+type countingWriter struct {
+	Count int
+}
+
+func (cw *countingWriter) Write(p []byte) (n int, err error) {
+	cw.Count ++
+	return len(p), nil
 }
